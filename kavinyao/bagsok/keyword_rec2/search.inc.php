335bfd7b7eb79b6e2bdc8e6049585dd0ef6111dc
kavinyao@gmail.com <kavinyao@gmail.com@3e40e070-b1dc-2f3f-f602-04bf03523387>
2011-10-19 01:39:08 +0000

    refactored a little for evaluating different methods more easily

---
+++ kavinyao/bagsok/keyword_rec2/search.inc.php
@@ -0,0 +1,79 @@
+<?php
+require_once('aggregate_utility.inc.php');
+
+interface iKeywordSearcher{
+    /**
+     * Returns array of product-weight pair via given $keywords array.
+     */
+    public function getResult($keywords);
+}
+
+class AggregatedKeywordSearcher implements iKeywordSearcher{
+    private function get_product_list_via($keyword){
+        $query = "SELECT id, keyword_set, product FROM keywordset_product WHERE keyword_set LIKE '%$keyword%'";
+        $result = mysql_query($query);
+        $products = array();
+        while($row = mysql_fetch_array($result)){
+            $temp = array();
+            $temp['id'] = $row['id'];
+            $temp['keyword'] = $row['keyword_set'];
+            $temp['product'] = $row['product'];
+            $products[] = $temp;
+        }
+
+        return $products;
+    }
+
+    private function remove_duplicate(){
+        $id_cache = array();
+        return function($item) use (&$id_cache){
+            $is_in = in_array($item['id'], $id_cache);
+            if(!$is_in)
+                $id_cache[] = $item['id'];
+            return !$is_in;
+        };
+    }
+
+    //use keywords size as weight
+    private function simple_weight($keyword_string){
+        $keyword_array = explode(' ', $keyword_string);
+        remove_empty2($keyword_array);
+        $weight = count($keyword_array);
+        return $weight;
+    }
+
+
+    public function getResult($keywords){
+        $all_result = array();
+        foreach($keywords as $keyword){
+            $result = $this->get_product_list_via($keyword);
+            $all_result = array_merge($result, $all_result);
+        }
+        $dup_remover = $this->remove_duplicate();
+        $filtered_result = array_filter($all_result, $this->remove_duplicate());
+
+        //combine weight
+        $product_weight_raw = array();
+        foreach($filtered_result as $item){
+            $weight = $this->simple_weight($item['keyword']);
+
+            if(array_key_exists($item['product'], $product_weight_raw))
+                $product_weight_raw[$item['product']] += $weight;
+            else
+                $product_weight_raw[$item['product']] = $weight;
+        }
+
+        //reorganize for sort
+        $product_weight = array();
+        foreach($product_weight_raw as $product => $weight)
+            $product_weight[] = array($product, $weight);
+
+        //sort by weight descendingly
+        usort($product_weight, function($item1, $item2){
+            return $item2[1] - $item1[1];
+        });
+
+        return $product_weight;
+    }
+}
+?>
